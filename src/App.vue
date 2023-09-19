f23509a32835516105c73eac1e33ec348e70e8ff
Jeka <jonrovniy@yandex.ru>
2023-09-19 20:29:44 +0300

    init proj

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div class="d-flex align-center justify-center">
+    <div>My template with router vuex and vuetify</div>
+    <v-btn>Send</v-btn>
+    <span> - vuetify</span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
